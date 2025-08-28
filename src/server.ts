1338b8cec68fc72c14dedeb744659433c088536b
victor <victor.vrigstad@gmail.com>
2025-08-28 22:49:09 +0200

    online server?7, working?

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
